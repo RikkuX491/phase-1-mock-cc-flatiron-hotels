4d1e4611563749d73cacb11c6225ff8854146001
Ricardo Guerra <RikkuG491@gmail.com>
2023-10-18 17:29:49 -0400

    Added solution code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 // your code here
+const commentListElement = document.getElementById('comment-list')
+commentListElement.innerHTML = ""
+const hotelListElement = document.getElementById('hotel-list')
+hotelListElement.innerHTML = ""
+
+fetch('http://localhost:3000/hotels')
+.then(response => response.json())
+.then(hotels => {
+    displayHotelDetails(hotels[0])
+
+    hotels.forEach(hotel => {
+        const liElement = document.createElement('li')
+        liElement.textContent = hotel.name
+        hotelListElement.appendChild(liElement)
+    })
+})
+
+function displayHotelDetails(hotel){
+    const hotelNameElement = document.getElementById('hotel-name')
+    hotelNameElement.textContent = hotel.name
+    const hotelImageElement = document.getElementById('hotel-image')
+    hotelImageElement.src = hotel.image_url
+    const hotelDescriptionElement = document.getElementById('hotel-description')
+    hotelDescriptionElement.textContent = hotel.description
+
+    hotel.comments.forEach(comment => {
+        addComment(comment)
+    })
+}
+
+function addComment(comment){
+    const liElement = document.createElement('li')
+    liElement.textContent = comment
+    commentListElement.appendChild(liElement)
+}
+
+const commentFormElement = document.getElementById('comment-form')
+commentFormElement.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const commentTextAreaElement = document.getElementById('comment')
+    addComment(commentTextAreaElement.value)
+})
